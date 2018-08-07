c3c6e39a0d929f89c02b2c7cb08c1fcf37848788
france14331 <59070042@it.kmitl.ac.th>
2018-08-07 12:24:04 +0700

    add javascript

--- Unit-1/Students/59070042/Lab-1/js.js
+++ Unit-1/Students/59070042/Lab-1/js.js
@@ -1,3 +1,3 @@
-username = prompt('What your name!');
-
+username = prompt('What your father name!');
+document.querySelector('h1').innerText = 'Welcome ' + username;
 // write js to display username in #welcome -- hint! --> querySelector
