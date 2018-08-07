c8f20a07dff7ebe0ca9a4b0294942b1d8ac3b7b4
Munawang <59070036@it.kmitl.ac.th>
2018-08-07 12:24:48 +0700

    เรียก username

--- Unit-1/Students/59070036/Lab-1/js.js
+++ Unit-1/Students/59070036/Lab-1/js.js
@@ -1,3 +1,3 @@
-username = prompt('What your name!');
-
+username = prompt('What is your name !');
+ document.querySelector('h1').innerText = "Welcome " + username;
 // write js to display username in #welcome -- hint! --> querySelector
