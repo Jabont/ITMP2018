16fdc265d9c73fbd94b724dc2142a45d7d6d15c7
it59070034 <42163063+it59070034@users.noreply.github.com>
2018-08-07 12:23:59 +0700

    Get Username
    เอา username มาใส่ แก้ใน js

--- Unit-1/Students/59070034/Lab-1/js.js
+++ Unit-1/Students/59070034/Lab-1/js.js
@@ -1,3 +1,4 @@
-username = prompt('What your name!');
+username = prompt('WHO R U!');
+document.querySelector('h1').innerText = 'Welcome '+username;
 
 // write js to display username in #welcome -- hint! --> querySelector
