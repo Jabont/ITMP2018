3cf2ff5b1b7cc33754fa8f7236ad034ad4074e03
Krammii <59070002@it.kmitl.ac.th>
2018-08-07 12:32:06 +0700

    editor
    - เชื่อม CSS
    - Animation
    - เชื่อม JS (script)

--- Unit-1/Students/59070002/Lab-1/js.js
+++ Unit-1/Students/59070002/Lab-1/js.js
@@ -1,3 +1,4 @@
-username = prompt('What your name!');
+username = prompt('What isss');
+document.querySelector('h1').innerText = "Welcome" + username
 
 // write js to display username in #welcome -- hint! --> querySelector
