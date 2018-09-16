4b56b2def489dd83d257942efb8f070337b0dbe4
france14331 <59070042@it.kmitl.ac.th>
2018-09-16 20:31:22 +0700

    Insert & Query

--- Production/59070042/animalfarm/addpet.php
+++ Production/59070042/animalfarm/addpet.php
@@ -27,18 +27,51 @@
 		$conn = conn();
 		$pet_name = $_POST['pet_name'];
 		$pet_caption = $_POST['pet_caption'];
-		$sql = "INSERT INTO pet (pet_name,pet_caption)
-		VALUES ('$pet_name','$pet_caption')";
+		$pet_video = $_POST['pet_video'];
+		$sql = "INSERT INTO pet (pet_name,pet_caption,pet_video)
+		VALUES ('$pet_name','$pet_caption','$pet_video')";
 		$conn->query($sql);
 	 	echo "ได้รับข้อมูลแล้ว";
-	 } 
+	 }
 	 ?>
 
 	<form method="POST">
 		<input type="text" name="pet_name" placeholder="Pet's Name">
 		<input type="text" name="pet_caption" placeholder="Pet's Caption">
+		<input type="text" name="pet_video" placeholder="Pet's Video">
 		<button name="send">Submit</button>
 	</form>
+
+	<div><br><br>
+	<table border="1px">
+		<tr>
+			<th>Pet's ID</th>
+			<th>Pet's Name</th>
+			<th>Pet's Caption</th>
+			<th>Pet's Video</th>
+		</tr>
+		<tr>
+			<?php
+				$conn = conn();  
+				$sql = "SELECT * FROM pet";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+    				// output data of each row
+    				while($row = $result->fetch_assoc()) {
+    					?>
+        				<td><?=$row["pet_id"]?></td>
+        				<td><?=$row["pet_name"]?></td>
+        				<td><?=$row["pet_caption"]?></td>
+        				<td><?=$row["pet_video"]?></td>
+    				<?php
+    				}
+				} else {
+    				echo "0 results";
+				}
+			?>
+		</tr>
+	</table>
+	</div>
 
 </body>
 </html>
--- Production/59070042/animalfarm/index.php
+++ Production/59070042/animalfarm/index.php
@@ -14,3 +14,14 @@
 		}
 	}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Animal Farm</title>
+</head>
+<body>
+	<a href="addpet.php">Add Pet</a>
+
+</body>
+</html>
