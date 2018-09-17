6bd51c78a0741c9cf1c17baf9687fea1dc2df4d7
Jabont Chamikorn <ibont.kurogo@gmail.com>
2018-09-17 11:14:08 +0700

    aadd form pfon

--- Unit-6/Master/index.php
+++ Unit-6/Master/index.php
@@ -20,7 +20,33 @@
 			return $conn;
 		}
 	}
+
+	$menu = ['BBQ','Sandwich','Pizza','Burger','Noodle'];
+	$price = [];
+	$price['BBQ'] = 30;
+	$price['Sandwich'] = 15;
+	$price['Pizza'] = 50;
+	$price['Burger'] = 40;
+	$price['Noodle'] = 35;
 	?>
+
+	<form method="POST">
+		<select name="order_name">
+			<?php
+			for ($i=0; $i < sizeof($menu); $i++) { 
+				?>
+				<option value="<?=$menu[$i]?>"><?=$menu[$i]?></option>
+				<?php
+			} 
+			?>
+		</select>
+		<input	type="text" name="order_number" placeholder="Number">
+		<input	type="text" name="order_price" placeholder="Price">
+		<input	type="text" name="c_name" placeholder="Customer's Name">
+		<button name="send" value="send_order">Order Now!</button>
+		
+	</form>
+
 	<?php
 	$conn = conn();
 	$sql = "SELECT * FROM orders";
