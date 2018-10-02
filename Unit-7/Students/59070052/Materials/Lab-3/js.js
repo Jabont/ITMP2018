651c2ad85882768ba017818118a6356b4bf5039b
90lfnp <59070052@it.kmitl.ac.th>
2018-10-02 12:11:28 +0700

    rainbow

--- Unit-7/Students/59070052/Materials/Lab-3/js.js
+++ Unit-7/Students/59070052/Materials/Lab-3/js.js
@@ -1,8 +1,13 @@
 b = document.querySelectorAll('div');
+c = document.querySelectorAll('.card');
 color = 0;
 plus = 360 / b.length;
+
 for (i = 0; i<b.length;i++){
 	b[i].style.background = "hsl("+color+",100%,80%)";
 	color += plus;
+	c[i].style.borderRadius = ""+c[i].getAttribute('round')+"px";
+
+
 
 };
