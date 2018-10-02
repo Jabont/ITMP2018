87e13df77825fa2703c2d7e07d3ef450f0f23333
OKKachasom <59070001@kmitl.ac.th>
2018-10-02 12:18:32 +0700

    more done

--- Unit-7/Students/59070001/Lab-3/js.js
+++ Unit-7/Students/59070001/Lab-3/js.js
@@ -1,4 +1,6 @@
 b = document.querySelectorAll('div');
+c = document.querySelectorAll('.card');
+
 first = 270;
 allb = b.length;
 second = first;
@@ -7,6 +9,13 @@ for (i = 0; i<b.length; i++) {
 	console.log("i = " + i);
 	b[i].style.background = "hsl("+(second)+",100%,80%)";
 	second = second - n;
-	
 	console.log("color = "+ second);
+
+
 };
+
+for (i = 0; i<c.length; i++){
+	round = c[i].getAttribute('round');
+	c[i].style.borderRadius = round + 'px';
+}
+
