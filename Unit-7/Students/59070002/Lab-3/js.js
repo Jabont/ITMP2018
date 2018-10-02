cc22858a0d0b446dd868cfc2251949162cc061c5
FAHKRAM <59070002@it.kmitl.ac.th>
2018-10-02 12:00:01 +0700

    radius round (lab3)

--- Unit-7/Students/59070002/Lab-3/js.js
+++ Unit-7/Students/59070002/Lab-3/js.js
@@ -1,6 +1,15 @@
 b = document.querySelectorAll('div');
-purple = 270; bg=0;
+card = document.querySelectorAll('.card');
+
+purple = 270; bg=0; 
 for (i = 0; i<b.length; i++) {
-	b[i].style.background = "hsl("+(purple-bg)+",100%,80%)"
-	bg = bg-5;
+	b[i].style.background = "hsl("+(purple-bg)+",100%,80%)";
+	bg = bg+10;
 };
+for (i = 0; i<card.length; i++) {
+	b[i].style.background = "hsl("+(purple-bg)+",100%,80%)";
+	card[i].style.borderRadius = card[i].getAttribute('round') + "px";
+
+};
+
+
