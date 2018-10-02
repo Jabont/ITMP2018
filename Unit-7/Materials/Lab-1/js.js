74225d6818e90ae2212e381c1c89d6cfb8302f71
Jabont Chamikorn <ibont.kurogo@gmail.com>
2018-10-02 11:01:47 +0700

    bg cOLOR

---
+++ Unit-7/Materials/Lab-1/js.js
@@ -0,0 +1,4 @@
+b = document.querySelectorAll('div');
+for (i = 0; i<b.length; i++) {
+	console.log(i);
+};
---
+++ Unit-7/Students/Master/Lab-3/js.js
@@ -0,0 +1,4 @@
+b = document.querySelectorAll('div');
+for (i = 0; i<b.length; i++) {
+	b[i].style.background = "hsl("+(i*5)+",100%,80%)"
+};
